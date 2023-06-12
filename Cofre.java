89667029d4e98aeea0c8bc4897f24ea60ba23fe3
João Elias Fadel <j.elias5@me.com>
2023-06-12 14:11:46 -0300

    Adicionada as classes Cofre e Hacker

--- Cofre.java
+++ Cofre.java
@@ -1,1 +1,14 @@
-//Jef
+import java.util.Random;
+
+public class Cofre {
+    private int senha;
+
+    public Cofre() {
+        Random random = new Random();
+        senha = random.nextInt(100000); // Gera um número aleatório entre 0 e 99999
+    }
+
+    public synchronized boolean verificarSenha(int tentativa) {
+        return tentativa == senha;
+    }
+}
--- Hacker.java
+++ Hacker.java
@@ -1,1 +1,24 @@
-//Jef
+public class Hacker extends Thread {
+    private Cofre cofre;
+    private boolean senhaEncontrada;
+
+    public Hacker(Cofre cofre) {
+        this.cofre = cofre;
+        this.senhaEncontrada = false;
+    }
+
+    @Override
+    public void run() {
+        for (int tentativa = 0; tentativa < 100000; tentativa++) {
+            if (cofre.verificarSenha(tentativa)) {
+                senhaEncontrada = true;
+                System.out.println("Hacker " + getId() + " abriu o cofre!");
+                break;
+            }
+        }
+    }
+
+    public boolean senhaEncontrada() {
+        return senhaEncontrada;
+    }
+}
